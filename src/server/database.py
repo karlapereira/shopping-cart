29c05fef0fd3efa2cf8af9738a35191ded26f7ff
karlapereira <karlapereira@gec.inatel.br>
2022-09-17 08:07:58 -0300

    update comment

--- src/server/database.py
+++ src/server/database.py
@@ -15,6 +15,7 @@ class DataBase:
 db = DataBase()
 
 async def connect_db():
+    # conexao mongo, com no máximo 10 conexões async
     db.client = AsyncIOMotorClient(
         db.database_uri,
         maxPoolSize=10,
