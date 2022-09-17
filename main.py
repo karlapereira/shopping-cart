8835533d30131dd5ee6a12b8da38129d53f4d520
karlapereira <karlapereira@gec.inatel.br>
2022-09-17 15:50:05 -0300

    exemplo imports da atividade

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
 
 from src.controllers.users import users_crud
-
+# from src.controllers.products import products_crud
+# from src.controllers.carrinho import carrinho_crud
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(users_crud())
